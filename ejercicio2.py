a9327ff81be9732108fb287a0f91678589acbe27
Lucas8276 <lucasnahuquin@gmail.com>
2024-04-23 17:03:54 -0300

    a seguir con ejer 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,31 +1,17 @@
 import numpy as np  # Import NumPy library
 
-def g(x):
-  return np.log(x**2) - 0.7  # Difference between f(x) and 0.7
+def funcionquetengo(x):
+    return np.log(x**2)-0.7
 
-def falsa_posicion(a, b, tol=1e-6, max_iter=100):
-  for i in range(max_iter):
-    if abs(g(b)) < tol:
-      return b
-    x_r = b - ((g(b) * (b - a)) / (g(b) - g(a)))
-    if g(a) * g(x_r) < 0:
-      b = x_r
-    else:
-      a = x_r
-  raise Exception("No se pudo encontrar la raíz en el número máximo de iteraciones")
+def metodoFalsaPosicion(extremo_inferior, extremo_superior):
+  valor= extremo_superior-(funcionquetengo(extremo_superior)*(extremo_superior-extremo_inferior))/(funcionquetengo(extremo_superior)-funcionquetengo(extremo_inferior))
+  return valor
 
-# Define f(x) using NumPy for clarity (not strictly necessary)
-def f(x):
-  return np.log(x**2)
+extremo_inferior = 0.5
+extremo_superior = 2
 
-# Values for initial guess
-a = 1.5
-b = 2
 
-# Find the root
-raiz = falsa_posicion(a, b)
+resultado= metodoFalsaPosicion(extremo_superior,extremo_inferior)
 
-# Print the result
-print("Raíz:", raiz)
-
+print("Resultado",resultado)
 
